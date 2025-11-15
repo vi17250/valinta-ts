a5cd656811c5ce82dc7b42f84c9fa4d009876386
vi17250 <v.aubinaud@protonmail.com>
2025-11-15 11:52:05 +0100

    test: 💍 test coverage is 100%
    ✅ Closes:  #2

---
+++ format.test.ts
@@ -0,0 +1,50 @@
+import { Colors } from "@neabyte/deno-ansi";
+import { assertEquals } from "@std/assert";
+
+import type { Option } from "./mod.ts";
+import { format } from "./format.ts";
+
+Deno.test(function it_returns_nothing_checked() {
+  const input: Option[] = [{
+    checked: false,
+    highlighted: true,
+    value: "a",
+  }, {
+    checked: false,
+    highlighted: false,
+    value: "b",
+  }];
+  
+  const expected: string[] = [`${Colors.bgRgb(`[ ] a`, 85, 170, 85)}`, "[ ] b"];
+  assertEquals(format(input), expected);
+});
+
+Deno.test(function it_returns_first_value_checked() {
+  const input: Option[] = [{
+    checked: true,
+    highlighted: true,
+    value: "a",
+  }, {
+    checked: false,
+    highlighted: false,
+    value: "b",
+  }];
+  
+  const expected: string[] = [`${Colors.bgRgb(`[x] a`, 85, 170, 85)}`, "[ ] b"];
+  assertEquals(format(input), expected);
+});
+
+Deno.test(function it_returns_second_value_checked() {
+  const input: Option[] = [{
+    checked: false,
+    highlighted: true,
+    value: "a",
+  }, {
+    checked: true,
+    highlighted: false,
+    value: "b",
+  }];
+  
+  const expected: string[] = [`${Colors.bgRgb(`[ ] a`, 85, 170, 85)}`, "[x] b"];
+  assertEquals(format(input), expected);
+});
