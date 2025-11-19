39768be2e97e10a1b5e275d851593fed9323df72
vi17250 <v.aubinaud@protonmail.com>
2025-11-19 20:54:59 +0100

    refactor: 💡 given when then in tests

--- util.test.ts
+++ util.test.ts
@@ -4,50 +4,104 @@ import { numberOfLines } from "./util.ts";
 
 // Array
 Deno.test(function it_return_same_array() {
+  // Given
   const input = [1, 2, 3];
-  assertEquals(array(input, 0), [1, 2, 3]);
+
+  // When
+  const offsetted = array(input, 0);
+
+  // Then
+  assertEquals(offsetted, [1, 2, 3]);
 });
 
 Deno.test(function it_offset_array_from_one() {
+  // Given
   const input = [1, 2, 3];
-  assertEquals(array(input, 1), [2, 3, 1]);
+
+  // When
+  const offsetted = array(input, 1);
+
+  // Then
+  assertEquals(offsetted, [2, 3, 1]);
 });
 
 Deno.test(function it_offset_array_from_two() {
+  // Given
   const input = ["a", "b", "c", "d"];
-  assertEquals(array(input, 2), ["c", "d", "a", "b"]);
+
+  // When
+  const offsetted = array(input, 2);
+
+  // Then
+  assertEquals(offsetted, ["c", "d", "a", "b"]);
 });
 
 Deno.test(function it_offset_array_from_three() {
+  // Given
   const input = ["a", "b", "c", "d"];
-  assertEquals(array(input, 3), ["d", "a", "b", "c"]);
+
+  // When
+  const offsetted = array(input, 3);
+
+  // Then
+  assertEquals(offsetted, ["d", "a", "b", "c"]);
 });
 
 Deno.test(function it_offset_array_by_revert() {
+  // Given
   const input = ["a", "b", "c", "d"];
-  assertEquals(array(input, -1), ["d", "a", "b", "c"]);
+
+  // When
+  const offsetted = array(input, -1);
+
+  // Then
+  assertEquals(offsetted, ["d", "a", "b", "c"]);
 });
 
 Deno.test(function it_split_and_offset_array_from_three() {
+  // Given
   const input = ["a", "b", "c", "d"];
-  assertEquals(array(input, 3, 2), ["d", "a"]);
+
+  // When
+  const offsetted = array(input, 3, 2);
+
+  // Then
+  assertEquals(offsetted, ["d", "a"]);
 });
 
 // Number of lines
 Deno.test(function it_returns_one() {
+  // Given
   const input = "Une simple ligne";
   const width = 30;
-  assertEquals(numberOfLines(input, width), 1);
+
+  // When
+  const number = numberOfLines(input, width);
+
+  // Then
+  assertEquals(number, 1);
 });
 
 Deno.test(function it_returns_two() {
+  // Given
   const input = "Une simple ligne\nEt une autre";
   const width = 30;
-  assertEquals(numberOfLines(input, width), 2);
+
+  // When
+  const number = numberOfLines(input, width);
+
+  // Then
+  assertEquals(number, 2);
 });
 
 Deno.test(function it_returns_three() {
+  // Given
   const input = "14 char length\n14 char length";
   const width = 10;
-  assertEquals(numberOfLines(input, width), 4);
+
+  // When
+  const number = numberOfLines(input, width);
+
+  // Then
+  assertEquals(number, 4);
 });
