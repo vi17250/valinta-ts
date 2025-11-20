8068a1fd9963be8e0741ec04e612b4a2c65559df
vi17250 <v.aubinaud@protonmail.com>
2025-11-20 18:49:07 +0100

    docs: ✏️ add JSDoc
    ✅ Closes: #12

--- format.ts
+++ format.ts
@@ -1,5 +1,15 @@
+/**
+ * A module that formats options to be displayed
+ * @module
+ */
 import type { Option } from "./mod.ts";
 import { Colors } from "@neabyte/deno-ansi";
+
+/**
+ * For each options, add checkbox and highlights the current option
+ * @param {Option[]} options
+ * @returns {string[]}
+ */
 
 export function format(options: Option[]): string[] {
   return options.map((option) => {
--- mod.ts
+++ mod.ts
@@ -1,3 +1,9 @@
+/**
+ * A tool for multiple selection in CLI
+ * Written for Deno runtime ⭐
+ * @module
+ */
+
 import { array, numberOfLines } from "./util.ts";
 import { display, getwidth, keyPressEvent } from "./cli.ts";
 import { KeyEnum, keyPressMapper } from "./keyPressMapper.ts";
@@ -5,14 +11,25 @@ import { useKey } from "./useKey.ts";
 import { Cursor, Terminal } from "@neabyte/deno-ansi";
 import { format } from "./format.ts";
 
+/**
+ * An interface that describe the data used in this lib
+ */
 export interface Option {
   checked: boolean;
   highlighted: boolean;
   value: string;
 }
 
+/**
+ * The number of lines rendered in the terminal
+ */
 const numberToRender: number = 11;
 
+/**
+ * Calls the library, display the data passed as argument and allows the user to select some of it
+ * @param {string[]} values to display
+ * @returns {Promise<string[]>} values selected
+ */
 export async function Select(values: string[]): Promise<string[]> {
   let options: Option[] = values.map((value: string, index: number) => {
     const highlighted = index == 0;
